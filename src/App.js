6bdb5afecabda2f93fc2c1a9cd5bdfbde3762131
avehghasemian <av.ghasemian@gmail.com>
2021-10-22 09:14:47 +0200

    axios requests

---
+++ src/App.js
@@ -0,0 +1,17 @@
+ //import React, { useState } from 'react';
+import SearchData from './components/SearchData';
+
+import "./App.css";
+
+
+const App = () => {
+
+// const [currentPage, setCurrenPage] = useState("SearchData")
+
+
+  return (
+    <div><SearchData  /></div>
+  );
+}
+
+export default App;
---
+++ src/components/SearchData.js
@@ -0,0 +1,64 @@
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+ import classes from "./SearchData.module.css";
+
+
+const SearchData = () => {
+const [quote, setQuote] = useState();
+const [character, setCharacter] = useState();
+
+    useEffect(() => {
+      
+    const headers = {
+      Accept: "application/json",
+      Authorization: "Bearer daNXH8aks1JeT2dEenRY",
+    }; 
+    const fetchData = async () => {
+     try {
+         const response = await axios.get("https://the-one-api.dev/v2/quote", {
+             headers: headers 
+         })
+         const quotes = await response.data;
+
+        const resQuote = quotes.docs[Math.floor(Math.random() * quotes.docs.length)]
+        setQuote(resQuote.dialog)
+
+        const secondResponse = await axios.get(
+          "https://the-one-api.dev/v2/character?_id=" + resQuote.character, {
+              headers: headers
+          });
+         const characters = await secondResponse.data;
+        console.log(secondResponse.data);
+
+        const resCharacter = characters.docs[0] 
+          setCharacter(resCharacter.name)
+     }catch(error) {
+        console.log("Error of Quote" + error );
+        console.log("Error of Character" + error );
+       }
+    }
+       fetchData()
+    }, [])
+return (
+
+    <div className={classes.dataWrapper}>
+      <blockquote className={classes.blockQuote}>{quote}</blockquote>
+      <cite>- {character}</cite>
+    </div>
+)
+
+}
+
+ export default SearchData;
+
+
+// const fetchData = async (searchTerm) => {
+//   const response = await axios.get("http://www.omdbapi.com/", {
+//     params: {
+//       apikey: "75cc1fba",
+//       s: "searchTerm",
+//     },
+//   });
+
+//   console.log(response.data);
+// };
