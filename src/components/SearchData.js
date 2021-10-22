27718e7fa9c47eb3832524e410c83f286cf1eb57
avehghasemian <av.ghasemian@gmail.com>
2021-10-22 18:58:12 +0200

    API optimisation

--- src/components/SearchData.js
+++ src/components/SearchData.js
@@ -1,67 +1,103 @@
-import axios from "axios";
 import React, { useState, useEffect } from "react";
- import classes from "./SearchData.module.css";
-
+import axios from "axios";
+import classes from "./SearchData.module.css";
 
 const SearchData = () => {
-const [quote, setQuote] = useState();
-const [character, setCharacter] = useState();
+  const [quote, setQuote] = useState();
+  const [character, setCharacter] = useState();
 
-    useEffect(() => {
-   
+  useEffect(() => {
     const headers = {
       Accept: "application/json",
       Authorization: "Bearer daNXH8aks1JeT2dEenRY",
-    }; 
+    };
     const fetchData = async () => {
-     try {
-         const response = await axios.get(
-           "https://the-one-api.dev/v2/quote",
-           {
-             headers: headers,
-           }
-         );
-         const datas = await response.data;
+      try {
+        const response = await axios.get("https://the-one-api.dev/v2/quote", {
+          headers: headers,
+        });
+        const datas = await response.data;
 
-        const resQuote = datas.docs[Math.floor(Math.random() * datas.docs.length)]
+        const resQuote =
+          datas.docs[Math.floor(Math.random() * datas.docs.length)];
         setQuote(resQuote.dialog);
 
         const secondResponse = await axios.get(
-          "https://the-one-api.dev/v2/character?_id=" + resQuote.character, {
-              headers: headers
-          });
-         const characters = await secondResponse.data;
+          "https://the-one-api.dev/v2/character?name!=Frodo" + resQuote.character,
+          {
+            headers: headers,
+          }
+        );
+        const characters = await secondResponse.data;
         console.log(secondResponse.data);
 
-        const resCharacter = characters.docs[0]
-          setCharacter(resCharacter.characters)
-     }catch(error) {
-        console.log("Error of Quote" + error );
-        console.log("Error of Character" + error );
-       }
-    }
-       fetchData()
-    }, [])
-return (
-
+        const resCharacter = characters.docs[0];
+        setCharacter(resCharacter.name);
+      } catch (error) {
+        console.log("Error of Quote" + error);
+        console.log("Error of Character" + error);
+      }
+    };
+    fetchData();
+  }, []);
+  return (
     <div className={classes.dataWrapper}>
       <blockquote className={classes.blockQuote}>{quote}</blockquote>
       <cite>- {character}</cite>
     </div>
-)
+  );
+};
 
-}
+export default SearchData;
 
- export default SearchData;
+// import React, { useState, useEffect } from "react";
+// import axios from "axios";
+// import classes from "./SearchData.module.css";
 
+// const SearchData = () => {
+//   const [quote, setQuote] = useState();
+//   const [character, setCharacter] = useState();
 
-// const fetchData = async (searchTerm) => {
-//   const response = await axios.get("http://www.omdbapi.com/", {
-//     params: {
-//       apikey: "75cc1fba",
-//       s: "searchTerm",
-//     },
-//   });
+//   useEffect(() => {
+//     const headers = {
+//       Accept: "application/json",
+//       Authorization: "Bearer daNXH8aks1JeT2dEenRY",
+//     };
+//     const fetchData = async () => {
+//       try {
+//         const response = await axios.get("https://the-one-api.dev/v2/quote", {
+//           headers: headers,
+//         });
+//         const datas = await response.data;
 
-//   console.log(response.data);
+//         const resQuote =
+//           datas.docs[Math.floor(Math.random() * datas.docs.length)];
+//         setQuote(resQuote.dialog);
+
+//         const secondResponse = await axios.get(
+//           "https://the-one-api.dev/v2/character?_id=" + resQuote.character,
+//           {
+//             headers: headers,
+//           }
+//         );
+//         const characters = await secondResponse.data;
+//         console.log(secondResponse.data);
+
+//         const resCharacter = characters.docs[0];
+//         setCharacter(resCharacter.name);
+//       } catch (error) {
+//         console.log("Error of Quote" + error);
+//         console.log("Error of Character" + error);
+//       }
+//     };
+//     fetchData();
+//   }, []);
+//   return (
+//     <div className={classes.dataWrapper}>
+//       <blockquote className={classes.blockQuote}>{quote}</blockquote>
+//       <cite>- {character}</cite>
+//     </div>
+//   );
 // };
+
+// export default SearchData;
